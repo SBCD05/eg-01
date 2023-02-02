01ae6a32724d9b995c0eccce65857315c6d78a26
SBCD05 <learning.testing.27>
2023-02-02 11:35:31 +0530

    Intial comment

---
+++ src/eg/Program1.java
@@ -0,0 +1,13 @@
+package eg;
+
+public class Program1 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This is first program");
+		
+
+	}
+
+}
---
+++ src/eg/Program2.java
@@ -0,0 +1,12 @@
+package eg;
+
+public class Program2 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("This is second program");
+
+	}
+
+}
